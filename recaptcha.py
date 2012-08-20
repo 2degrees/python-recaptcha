302ac88ca0538c4b23d7aec62246cc6064618327
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2012-08-20 17:52:07 +0100

    Client implemented but not yet manually tested

---
+++ recaptcha.py
@@ -0,0 +1,246 @@
+################################################################################
+#
+# Copyright (c) 2012, 2degrees Limited <gustavonarea@2degreesnetwork.com>.
+# All Rights Reserved.
+#
+# This file is part of python-recaptcha <http://packages.python.org/recaptcha>,
+# which is subject to the provisions of the BSD at
+# <http://dev.2degreesnetwork.com/p/2degrees-license.html>. A copy of the
+# license should accompany this distribution. THIS SOFTWARE IS PROVIDED "AS IS"
+# AND ANY AND ALL EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST
+# INFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+################################################################################
+"""reCAPTCHA client."""
+
+from json import dumps as json_encode
+from urllib import urlencode
+from urllib2 import Request
+from urllib2 import URLError
+from urllib2 import urlopen
+from urlparse import urljoin
+from urlparse import urlsplit
+from urlparse import urlunsplit
+
+
+__all__ = [
+    'RecaptchaClient',
+    'RecaptchaException',
+    'RecaptchaInvalidChallengeError',
+    'RecaptchaInvalidPrivateKeyError',
+    'RecaptchaUnreachableError'
+    ]
+
+
+_RECAPTCHA_API_URL = 'http://www.google.com/recaptcha/api/'
+
+
+_RECAPTCHA_VERIFICATION_URL_PATH = 'verify'
+_RECAPTCHA_JAVASCRIPT_CHALLENGE_RELATIVE_URL_PATH = 'challenge'
+_RECAPTCHA_NOSCRIPT_CHALLENGE_RELATIVE_URL_PATH = 'noscript'
+
+
+_RECAPTCHA_CHALLENGE_MARKUP_TEMPLATE = """
+<script type="text/javascript">
+    var RecaptchaOptions = {recaptcha_options_json};
+</script>
+<script
+    type="text/javascript"
+    src="{javascript_challenge_url}"
+    >
+</script>
+<noscript>
+   <iframe
+       src="{noscript_challenge_url}"
+       height="300"
+       width="500"
+       frameborder="0"
+       >
+   </iframe>
+   <br />
+   <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+   <input
+       type="hidden"
+       name="recaptcha_response_field"
+       value="manual_challenge"
+       />
+</noscript>
+"""
+
+
+_CLIENT_USER_AGENT = \
+    '2degrees reCAPTCHA Client (https://github.com/2degrees/python-recaptcha)'
+
+
+class RecaptchaClient(object):
+    
+    def __init__(
+        self,
+        private_key,
+        public_key,
+        recaptcha_options=None,
+        verification_timeout=None,
+        ):
+        super(RecaptchaClient, self).__init__()
+        
+        self.private_key = private_key
+        self.public_key = public_key
+        
+        self.recaptcha_options_json = json_encode(recaptcha_options or {})
+        
+        self.verification_timeout = verification_timeout
+    
+    def get_challenge_markup(
+        self,
+        was_previous_solution_incorrect=False,
+        use_ssl=False,
+        ):
+        challenge_markup_variables = {
+            'recaptcha_options_json': self.recaptcha_options_json,
+            }
+        
+        challenge_urls = self._get_challenge_urls(
+            was_previous_solution_incorrect,
+            use_ssl,
+            )
+        challenge_markup_variables.update(challenge_urls)
+        
+        challenge_markup = _RECAPTCHA_CHALLENGE_MARKUP_TEMPLATE.format(
+            **challenge_markup_variables
+            )
+        return challenge_markup
+    
+    def is_solution_correct(self, solution_text, challenge_id, remote_ip):
+        if not solution_text or not challenge_id:
+            return False
+        
+        verification_result = self._get_recaptcha_response_for_solution(
+            solution_text,
+            challenge_id,
+            remote_ip,
+            )
+        
+        is_solution_correct_ = verification_result['is_solution_correct']
+        
+        if not is_solution_correct_:
+            error_code = verification_result['error_code']
+            if error_code == 'invalid-request-cookie':
+                raise RecaptchaInvalidChallengeError(challenge_id)
+            elif error_code == 'invalid-site-private-key':
+                raise RecaptchaInvalidPrivateKeyError(self.private_key)
+        
+        return is_solution_correct_
+    
+    def _get_challenge_urls(
+        self,
+        was_previous_solution_incorrect,
+        use_ssl,
+        ):
+        url_query_components = {'k': self.public_key}
+        if was_previous_solution_incorrect:
+            url_query_components['error'] = 'incorrect-captcha-sol'
+        url_query_encoded = urlencode(url_query_components)
+        
+        javascript_challenge_url = _get_recaptcha_api_call_url(
+            use_ssl,
+            _RECAPTCHA_JAVASCRIPT_CHALLENGE_RELATIVE_URL_PATH,
+            url_query_encoded,
+            )
+        
+        noscript_challenge_url = _get_recaptcha_api_call_url(
+            use_ssl,
+            _RECAPTCHA_NOSCRIPT_CHALLENGE_RELATIVE_URL_PATH,
+            url_query_encoded,
+            )
+        
+        challenge_urls = {
+            'javascript_challenge_url': javascript_challenge_url,
+            'noscript_challenge_url': noscript_challenge_url,
+            }
+        return challenge_urls
+    
+    def _get_recaptcha_response_for_solution(
+        self,
+        solution_text,
+        challenge_id,
+        remote_ip,
+        ):
+        verification_url = _get_recaptcha_api_call_url(
+            _RECAPTCHA_VERIFICATION_URL_PATH,
+            use_ssl=True,
+            )
+        request_data = urlencode({
+            'privatekey': self.private_key,
+            'remoteip': remote_ip,
+            'challenge': challenge_id,
+            'response': solution_text,
+            })
+        request = Request(
+            url=verification_url,
+            data=request_data,
+            headers={'User-agent': _CLIENT_USER_AGENT},
+            )
+        
+        urlopen_kwargs = {}
+        if self.verification_timeout is not None:
+            urlopen_kwargs['timeout'] = self.verification_timeout
+        try:
+            response = urlopen(request, **urlopen_kwargs)
+        except URLError, exc:
+            raise RecaptchaUnreachableError(exc)
+        else:
+            response_lines = response.read().splitlines()
+            response.close()
+        
+        is_solution_correct = response_lines[0] == 'true'
+        verification_result = {'is_solution_correct': is_solution_correct}
+        if not is_solution_correct:
+            verification_result['error_code'] = response_lines[1]
+        
+        return verification_result
+
+
+#{ Exceptions
+
+
+class RecaptchaException(Exception):
+    """Base class for all reCAPTCHA-related exceptions."""
+    pass
+
+
+class RecaptchaInvalidPrivateKeyError(RecaptchaException):
+    pass
+
+
+class RecaptchaInvalidChallengeError(RecaptchaException):
+    pass
+
+
+class RecaptchaUnreachableError(RecaptchaException):
+    pass
+
+
+#{ Utilities
+
+
+def _get_recaptcha_api_call_url(use_ssl, relative_url_path, encoded_query=''):
+    url_scheme = 'https' if use_ssl else 'http'
+    
+    recaptcha_api_url_components = urlsplit(_RECAPTCHA_API_URL)
+    url_path = urljoin(
+        recaptcha_api_url_components.path,
+        relative_url_path,
+        )
+    
+    url = urlunsplit((
+        url_scheme,
+        recaptcha_api_url_components.netloc,
+        url_path,
+        encoded_query,
+        '',
+        ))
+    return url
+
+
+#}
---
+++ test_recaptcha.py
@@ -0,0 +1,301 @@
+################################################################################
+#
+# Copyright (c) 2012, 2degrees Limited <gustavonarea@2degreesnetwork.com>.
+# All Rights Reserved.
+#
+# This file is part of python-recaptcha <http://packages.python.org/recaptcha>,
+# which is subject to the provisions of the BSD at
+# <http://dev.2degreesnetwork.com/p/2degrees-license.html>. A copy of the
+# license should accompany this distribution. THIS SOFTWARE IS PROVIDED "AS IS"
+# AND ANY AND ALL EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST
+# INFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+################################################################################
+
+from json import loads as json_decode
+from urlparse import parse_qs
+from urlparse import urlparse
+
+from nose.tools import assert_false
+from nose.tools import assert_in
+from nose.tools import assert_not_equal
+from nose.tools import assert_not_in
+from nose.tools import assert_raises_regexp
+from nose.tools import eq_
+from nose.tools import ok_
+
+from recaptcha import _RECAPTCHA_API_URL
+from recaptcha import RecaptchaClient
+from recaptcha import RecaptchaInvalidChallengeError
+from recaptcha import RecaptchaInvalidPrivateKeyError
+
+
+__all__ = [
+    'TestChallengeOptions',
+    'TestChallengeURLsGeneration',
+    'TestSolutionVerification',
+    ]
+
+
+#{ Challenge markup generation tests
+
+
+class TestChallengeURLsGeneration(object):
+    
+    def test_public_key_inclusion(self):
+        variables = _FAKE_CLIENT._get_challenge_urls(False, False)
+        
+        javascript_challenge_url = variables['javascript_challenge_url']
+        javascript_challenge_url_components = urlparse(javascript_challenge_url)
+        javascript_challenge_url_query = parse_qs(
+            javascript_challenge_url_components.query,
+            )
+        assert_in('k', javascript_challenge_url_query)
+        eq_(_FAKE_CLIENT.public_key, javascript_challenge_url_query['k'][0])
+        
+        noscript_challenge_url = variables['noscript_challenge_url']
+        noscript_challenge_url_components = urlparse(noscript_challenge_url)
+        eq_(
+            javascript_challenge_url_components.query,
+            noscript_challenge_url_components.query,
+            )
+    
+    def test_ssl_required(self):
+        variables = _FAKE_CLIENT._get_challenge_urls(
+            False,
+            use_ssl=False,
+            )
+        
+        javascript_challenge_url = variables['javascript_challenge_url']
+        ok_(javascript_challenge_url.startswith(_RECAPTCHA_API_URL))
+        
+        noscript_challenge_url = variables['noscript_challenge_url']
+        ok_(noscript_challenge_url.startswith(_RECAPTCHA_API_URL))
+    
+    def test_ssl_not_required(self):
+        variables = _FAKE_CLIENT._get_challenge_urls(
+            False,
+            use_ssl=True,
+            )
+        
+        javascript_challenge_url = variables['javascript_challenge_url']
+        ok_(javascript_challenge_url.startswith('https://'))
+        
+        noscript_challenge_url = variables['noscript_challenge_url']
+        ok_(noscript_challenge_url.startswith('https://'))
+    
+    def test_previous_solution_incorrect(self):
+        variables = _FAKE_CLIENT._get_challenge_urls(
+            was_previous_solution_incorrect=True,
+            use_ssl=False,
+            )
+        
+        javascript_challenge_url = variables['javascript_challenge_url']
+        javascript_challenge_url_components = urlparse(javascript_challenge_url)
+        javascript_challenge_url_query = parse_qs(
+            javascript_challenge_url_components.query,
+            )
+        assert_in('error', javascript_challenge_url_query)
+        eq_('incorrect-captcha-sol', javascript_challenge_url_query['error'][0])
+        
+        noscript_challenge_url = variables['noscript_challenge_url']
+        noscript_challenge_url_components = urlparse(noscript_challenge_url)
+        eq_(
+            javascript_challenge_url_components.query,
+            noscript_challenge_url_components.query,
+            )
+    
+    def test_previous_solution_correct(self):
+        variables = _FAKE_CLIENT._get_challenge_urls(
+            was_previous_solution_incorrect=False,
+            use_ssl=False,
+            )
+        
+        javascript_challenge_url = variables['javascript_challenge_url']
+        javascript_challenge_url_components = urlparse(javascript_challenge_url)
+        javascript_challenge_url_query = parse_qs(
+            javascript_challenge_url_components.query,
+            )
+        assert_not_in('error', javascript_challenge_url_query)
+        
+        noscript_challenge_url = variables['noscript_challenge_url']
+        noscript_challenge_url_components = urlparse(noscript_challenge_url)
+        eq_(
+            javascript_challenge_url_components.query,
+            noscript_challenge_url_components.query,
+            )
+    
+    def test_url_paths(self):
+        variables = _FAKE_CLIENT._get_challenge_urls(
+            was_previous_solution_incorrect=False,
+            use_ssl=False,
+            )
+        
+        javascript_challenge_url = variables['javascript_challenge_url']
+        javascript_challenge_url_components = urlparse(javascript_challenge_url)
+        
+        noscript_challenge_url = variables['noscript_challenge_url']
+        noscript_challenge_url_components = urlparse(noscript_challenge_url)
+        
+        assert_not_equal(
+            javascript_challenge_url_components.path,
+            noscript_challenge_url_components.path,
+            )
+
+
+class TestChallengeOptions(object):
+    
+    def test_options(self):
+        recaptcha_options = {'key1': 'value', 'key2': 3}
+        client = RecaptchaClient(
+            _FAKE_PRIVATE_KEY,
+            _FAKE_PUBLIC_KEY,
+            recaptcha_options=recaptcha_options,
+            )
+        
+        decoded_recaptcha_options = json_decode(client.recaptcha_options_json)
+        eq_(recaptcha_options, decoded_recaptcha_options)
+    
+    def test_no_options(self):
+        client = RecaptchaClient(_FAKE_PRIVATE_KEY, _FAKE_PUBLIC_KEY)
+        
+        decoded_recaptcha_options = json_decode(client.recaptcha_options_json)
+        assert_false(decoded_recaptcha_options)
+
+
+#}
+
+
+class TestSolutionVerification(object):
+    
+    def test_empty_solution(self):
+        client = _OfflineVerificationClient(_INCORRECT_SOLUTION_RESULT)
+        
+        is_solution_correct = client.is_solution_correct(
+            '',
+            _FAKE_CHALLENGE_ID,
+            _RANDOM_REMOTE_IP,
+            )
+        assert_false(is_solution_correct)
+        
+        # reCAPTCHA must not have been contacted
+        eq_(0, client.communication_attemps)
+    
+    def test_empty_challenge(self):
+        client = _OfflineVerificationClient(_INCORRECT_SOLUTION_RESULT)
+        
+        is_solution_correct = client.is_solution_correct(
+            _FAKE_SOLUTION_TEXT,
+            '',
+            _RANDOM_REMOTE_IP,
+            )
+        assert_false(is_solution_correct)
+        
+        # reCAPTCHA must not have been contacted
+        eq_(0, client.communication_attemps)
+    
+    def test_invalid_private_key(self):
+        invalid_private_key_result = {
+            'is_solution_correct': False,
+            'error_code': 'invalid-site-private-key',
+            }
+        client = _OfflineVerificationClient(invalid_private_key_result)
+        
+        with assert_raises_regexp(
+            RecaptchaInvalidPrivateKeyError,
+            client.private_key,
+            ):
+            client.is_solution_correct(
+                _FAKE_SOLUTION_TEXT,
+                _FAKE_CHALLENGE_ID,
+                _RANDOM_REMOTE_IP,
+                )
+    
+    def test_invalid_challenge(self):
+        invalid_challenge_result = {
+            'is_solution_correct': False,
+            'error_code': 'invalid-request-cookie',
+            }
+        client = _OfflineVerificationClient(invalid_challenge_result)
+        
+        with assert_raises_regexp(
+            RecaptchaInvalidChallengeError,
+            _FAKE_CHALLENGE_ID,
+            ):
+            client.is_solution_correct(
+                _FAKE_SOLUTION_TEXT,
+                _FAKE_CHALLENGE_ID,
+                _RANDOM_REMOTE_IP,
+                )
+    
+    def test_correct_solution_and_challenge(self):
+        correct_solution_result = {'is_solution_correct': True}
+        client = _OfflineVerificationClient(correct_solution_result)
+        
+        is_solution_correct = client.is_solution_correct(
+            _FAKE_SOLUTION_TEXT,
+            _FAKE_CHALLENGE_ID,
+            _RANDOM_REMOTE_IP,
+            )
+        ok_(is_solution_correct)
+    
+    def test_incorrect_solution(self):
+        client = _OfflineVerificationClient(_INCORRECT_SOLUTION_RESULT)
+        
+        is_solution_correct = client.is_solution_correct(
+            _FAKE_SOLUTION_TEXT,
+            _FAKE_CHALLENGE_ID,
+            _RANDOM_REMOTE_IP,
+            )
+        assert_false(is_solution_correct)
+
+
+#{ Mock objects and stubs
+
+
+_INCORRECT_SOLUTION_RESULT = {
+    'is_solution_correct': False,
+    'error_code': 'incorrect-captcha-sol',
+    }
+
+
+_FAKE_PRIVATE_KEY = 'private key'
+_FAKE_PUBLIC_KEY = 'public key'
+
+
+_FAKE_SOLUTION_TEXT = 'hello world'
+_FAKE_CHALLENGE_ID = '12345'
+_RANDOM_REMOTE_IP = '192.0.2.0'
+
+
+_FAKE_CLIENT = RecaptchaClient(
+    _FAKE_PRIVATE_KEY,
+    _FAKE_PUBLIC_KEY,
+    )
+
+
+class _OfflineVerificationClient(RecaptchaClient):
+    
+    def __init__(self, verification_result):
+        super(_OfflineVerificationClient, self).__init__(
+            _FAKE_PRIVATE_KEY,
+            _FAKE_PUBLIC_KEY,
+            )
+        
+        self.verification_result = verification_result
+        self.communication_attemps = 0
+    
+    def _get_recaptcha_response_for_solution(
+        self,
+        solution_text,
+        challenge_id,
+        remote_ip,
+        ):
+        
+        self.communication_attemps += 1
+        
+        return self.verification_result
+
+
+#}
